fd338a439ce6eeff6a390911aabf976db7601e4a
Vladislav <grishaewlad@gmail.com>
2023-04-26 20:48:49 +0300

    Исправлены категории в html

--- SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/MainController.java
+++ SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/MainController.java
@@ -48,12 +48,12 @@ public class MainController {
     }
 
     @GetMapping("/person account")
-    public String index(Model model){
+    public String index(Model model) {
         // Получаем объект аутентификации -> с помощью SpringContextHolder обращаемся к контексту и на нем вызываем метод аутентификации. Из сессии текущего пользователя получаем объект, который был положен в данную сессию после аутентификации пользователя
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         String role = personDetails.getPerson().getRole();
-        if(role.equals("ROLE_ADMIN")){
+        if (role.equals("ROLE_ADMIN")) {
             return "redirect:/admin";
         }
 //        System.out.println(personDetails.getPerson());
@@ -72,14 +72,14 @@ public class MainController {
 //    }
 
     @GetMapping("/registration")
-    public String registration(@ModelAttribute("person") Person person){
+    public String registration(@ModelAttribute("person") Person person) {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String resultRegistration(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+    public String resultRegistration(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
         personValidator.validate(person, bindingResult);
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "registration";
         }
         personService.register(person);
@@ -87,18 +87,18 @@ public class MainController {
     }
 
     @GetMapping("/person account/product/info/{id}")
-    public String infoProduct(@PathVariable("id") int id, Model model){
+    public String infoProduct(@PathVariable("id") int id, Model model) {
         model.addAttribute("product", productService.getProductId(id));
         return "/user/infoProduct";
     }
 
     @PostMapping("/person account/product/search")
-    public String productSearch(@RequestParam("search") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "contract", required = false, defaultValue = "")String contract, Model model){
+    public String productSearch(@RequestParam("search") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "contract", required = false, defaultValue = "") String contract, Model model) {
         model.addAttribute("products", productService.getAllProduct());
 
-        if(!ot.isEmpty() & !Do.isEmpty()){
-            if(!price.isEmpty()){
-                if(price.equals("sorted_by_ascending_price")) {
+        if (!ot.isEmpty() & !Do.isEmpty()) {
+            if (!price.isEmpty()) {
+                if (price.equals("sorted_by_ascending_price")) {
                     if (!contract.isEmpty()) {
                         if (contract.equals("furniture")) {
                             model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
@@ -110,17 +110,17 @@ public class MainController {
                     } else {
                         model.addAttribute("search_product", productRepository.findByTitleOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
                     }
-                } else if(price.equals("sorted_by_descending_price")){
-                    if(!contract.isEmpty()){
+                } else if (price.equals("sorted_by_descending_price")) {
+                    if (!contract.isEmpty()) {
                         System.out.println(contract);
-                        if(contract.equals("furniture")){
+                        if (contract.equals("furniture")) {
                             model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
-                        }else if (contract.equals("appliances")) {
+                        } else if (contract.equals("appliances")) {
                             model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
                         } else if (contract.equals("clothes")) {
                             model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
                         }
-                    }  else {
+                    } else {
                         model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
                     }
                 }
@@ -139,7 +139,7 @@ public class MainController {
     }
 
     @GetMapping("/cart/add/{id}")
-    public String addProductInCart(@PathVariable("id") int id, Model model){
+    public String addProductInCart(@PathVariable("id") int id, Model model) {
         // Получаем продукт по id
         Product product = productService.getProductId(id);
         // Извлекаем объект аутентифицированного пользователя
@@ -153,7 +153,7 @@ public class MainController {
     }
 
     @GetMapping("/cart")
-    public String cart(Model model){
+    public String cart(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         // Извлекаем id пользователя из объекта
@@ -163,13 +163,13 @@ public class MainController {
         List<Product> productList = new ArrayList<>();
 
         // Получаем продукты из корзины по id товара
-        for (Cart cart: cartList) {
+        for (Cart cart : cartList) {
             productList.add(productService.getProductId(cart.getProductId()));
         }
 
         // Вычисление итоговой цена
         float price = 0;
-        for (Product product: productList) {
+        for (Product product : productList) {
             price += product.getPrice();
         }
 
@@ -179,7 +179,7 @@ public class MainController {
     }
 
     @GetMapping("/cart/delete/{id}")
-    public String deleteProductFromCart(Model model, @PathVariable("id") int id){
+    public String deleteProductFromCart(Model model, @PathVariable("id") int id) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         // Извлекаем id пользователя из объекта
@@ -188,7 +188,7 @@ public class MainController {
         List<Product> productList = new ArrayList<>();
 
         // Получаем продукты из корзины по id товара
-        for (Cart cart: cartList) {
+        for (Cart cart : cartList) {
             productList.add(productService.getProductId(cart.getProductId()));
         }
         cartRepository.deleteCartByProductId(id);
@@ -196,7 +196,7 @@ public class MainController {
     }
 
     @GetMapping("/order/create")
-    public String order(){
+    public String order() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         // Извлекаем id пользователя из объекта
@@ -206,18 +206,18 @@ public class MainController {
         List<Product> productList = new ArrayList<>();
 
         // Получаем продукты из корзины по id товара
-        for (Cart cart: cartList) {
+        for (Cart cart : cartList) {
             productList.add(productService.getProductId(cart.getProductId()));
         }
 
         // Вычисление итоговой цена
         float price = 0;
-        for (Product product: productList) {
+        for (Product product : productList) {
             price += product.getPrice();
         }
 
         String uuid = UUID.randomUUID().toString();
-        for(Product product : productList){
+        for (Product product : productList) {
             Order newOrder = new Order(uuid, product, personDetails.getPerson(), 1, product.getPrice(), Status.Оформлен);
             orderRepository.save(newOrder);
             cartRepository.deleteCartByProductId(product.getId());
@@ -226,12 +226,12 @@ public class MainController {
     }
 
     @GetMapping("/orders")
-    public String orderUser(Model model){
+    public String orderUser(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         List<Order> orderList = orderRepository.findByPerson(personDetails.getPerson());
-         model.addAttribute("orders", orderList);
-         return "/user/orders";
+        model.addAttribute("orders", orderList);
+        return "/user/orders";
     }
 
 
